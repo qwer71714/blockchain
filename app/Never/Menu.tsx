5ab7a1e8dd2a0e6fa38973e94417b67f9102fc4c
verionik <qwer71714@naver.com>
2023-08-19 23:29:27 +0900

    어후 힘들다

--- app/Never/Menu.tsx
+++ app/Never/Menu.tsx
@@ -1,9 +1,41 @@
+import Image from "next/image";
 import React from "react";
+import BlockChain from "@/public/images/BlockChain-logo-no.png";
 
-export default function Menu() {
+const Menu = () => {
+  type NavLinkProps = {
+    href: string;
+    children: React.ReactNode;
+  };
+
+  const NavLink: React.FC<NavLinkProps> = ({ href, children }) => (
+    <a
+      href={href}
+      className="ps-8 font-normal hover:text-blue-600 duration-300"
+    >
+      {children}
+    </a>
+  );
   return (
-    <div className="border-b border-zinc-700">
-      <div className="container mx-auto py-5">박스</div>
+    <div className="border-b border-zinc-200">
+      <div className="container mx-auto py-4">
+        <div className="flex items-center">
+          <Image
+            src={BlockChain}
+            alt="Picture of the author"
+            width={46}
+            height={46}
+          />
+          <p className="pl-3 text-2xl font-bold">BlockChain</p>
+          <div className="flex justify-end ms-auto">
+            <NavLink href="/">Docs</NavLink>
+            <NavLink href="/">Puisw</NavLink>
+            <NavLink href="/">rrt</NavLink>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Menu;
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="container mx-auto max-[640px]:p-3">뭘봐</div>
+      <div className="container mx-auto max-[640px]:p-3"></div>
     </main>
   );
 }
