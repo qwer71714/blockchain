d3e555c31cd9b9260f659c738431062e96938524
verionik <qwer71714@naver.com>
2023-08-19 18:35:05 +0900

    올리기

---
+++ app/Never/Menu.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <div className="border-b border-zinc-700">
+      <div className="container mx-auto py-5">박스</div>
+    </div>
+  );
+}
