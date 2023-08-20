c22191e1f2c8e61f7292eeaf6caa337ad46bc137
supernova <seungmin10524@gmail.com>
2023-08-20 14:28:10 +0900

    뭔가 허전해서 넣은 프론트

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <main>
-      <div className="container mx-auto max-[640px]:p-3"></div>
+    <main className="center">
+      <div className="container mx-auto max-[640px]:p-3">
+        <h1 className="title">BlockChainTalk</h1>
+        <h1>For the fastest Internet</h1>
+      </div>
     </main>
   );
 }
