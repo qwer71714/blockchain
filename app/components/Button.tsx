7865a32f9cad506bc62a96e2b82adbabee48207b
verionik <qwer71714@naver.com>
2023-08-21 19:39:12 +0900

    app>page.tsx 16~17행 버튼색상 그대로 냅두고 수정금지

---
+++ app/components/Button.tsx
@@ -0,0 +1,53 @@
+import clsx from "clsx";
+
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined; // 버튼의 타입을 지정하는 속성
+  fullWidth?: boolean; // 버튼의 너비를 꽉 채우는지 여부를 지정하는 속성
+  children?: React.ReactNode; // 버튼 내부에 표시되는 내용
+  onClick?: () => void; // 버튼 클릭 시 실행되는 함수
+  secondary?: boolean; // 보조 버튼 스타일을 적용하는지 여부를 지정하는 속성
+  danger?: boolean; // 위험 버튼 스타일을 적용하는지 여부를 지정하는 속성
+  disabled?: boolean; // 버튼 비활성화 여부를 지정하는 속성
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type = "button",
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disabled,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(
+        `
+        rounded-md 
+        px-3 
+        py-2 
+        text-sm 
+        font-semibold 
+        focus-visible:outline 
+        focus-visible:outline-2 
+        focus-visible:outline-offset-2 
+        `,
+        disabled && "opacity-50 cursor-default", // 비활성화된 버튼일 경우 투명도와 커서 스타일을 변경
+        fullWidth && "w-full", // fullWidth 속성이 true일 경우 버튼의 너비를 100%로 설정
+        secondary ? "text-gray-900" : "text-white", // secondary 속성이 true일 경우 글자 색상을 회색으로, 그렇지 않을 경우 흰색으로 설정
+        danger &&
+          "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600", // danger 속성이 true일 경우 배경색과 호버 시 배경색, 포커스 시 아웃라인 색상을 분홍색으로 설정
+        !secondary &&
+          !danger &&
+          "bg-black hover:bg-stone-700 focus-visible:outline-stone-600" // secondary와 danger 속성이 모두 false일 경우 배경색과 호버 시 배경색, 포커스 시 아웃라인 색상을 하늘색으로 설정
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-import Image from "next/image";
+"use client";
+
 import "./globals.css";
+import Button from "@/app/components/Button";
 import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {});
   return (
-    <main className="mt-5">
-      <div className="flex justify-center h-screen">
-        <h1 className='text-5xl'>{`Block Chain Talk`}</h1>
+    <main className="container m-auto w-11/12">
+      <div className="mt-48 text-center">
+        <h1 className="text-6xl font-black">BlockChain Talk</h1>
+        <div className="mt-7">
+          <p className="text-lg">블록체인에서 오신 것을 환영합니다</p>
+          {/* 버튼 색상 그래도 냅두고 수정금지 */}
+          <div className="mt-12 flex justify-between w-56 mx-auto">
+            <Button>뭘봐</Button> 
+            <Button>뭘봐</Button>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
-
-
