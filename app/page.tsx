e7c5219ffb746c54d282e60ed85b18b9d21a0b95
supernova <seungmin10524@gmail.com>
2023-08-22 19:13:02 +0900

    픽셀사이즈 변경

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="container m-auto w-11/12">
       <div className="mt-40 text-center"> {/*세로 간격 수정*/}
         <h1 className="text-7xl font-black">BlockChain Talk</h1>
-        <h1 className="text-7xl text-blue-500">Empowering Unity, Fostering Growth</h1> {/*슬로건추가 변경가능*/}
+        <h1 className="text-5xl text-blue-500">Empowering Unity, Fostering Growth</h1> {/*슬로건추가 변경가능*/}
         <div className="mt-7">
           <p className="text-lg">블록체인에서 오신 것을 환영합니다.</p>
           {/* 버튼 냅두고 수정금지 */}
