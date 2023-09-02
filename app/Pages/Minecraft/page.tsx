a5323eee26da17654ed3f22ff1ed06bf46300916
supernova <seungmin10524@gmail.com>
2023-09-03 07:55:05 +0900

    아이디어 고갈

--- app/Pages/Minecraft/page.tsx
+++ app/Pages/Minecraft/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function page() {
   return <div className="pt-20 text-center dark:bg-black h-screen">
-      <h1 className="text-white text-7xl font-black">Error404</h1>
-      <h1 className="text-white text-5xl font-black">not found</h1>
+      <h1 className="text-white text-7xl font-bold">Error404</h1>
+      <h1 className="text-white text-5xl font-bold">not found</h1>
   </div>;
 }
