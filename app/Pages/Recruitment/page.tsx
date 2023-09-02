c458a21b7a66eeee0770974b96f44ad07dfcec11
supernova <seungmin10524@gmail.com>
2023-09-03 07:56:11 +0900

    실수로 삭제함

---
+++ app/Pages/Recruitment/page.tsx
@@ -0,0 +1,9 @@
+import ThemeSwitcher from "@/app/components/Theme/ThemeSwitcher";
+import React from "react";
+
+export default function page() {
+  return <div className="pt-20 text-center dark:bg-black h-screen">
+      <h1 className="text-white text-7xl font-bold">Error404</h1>
+      <h1 className="text-white text-5xl font-bold">not found</h1>
+  </div>;
+}
